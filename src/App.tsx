4b86bf56026f4bbca03ec8a80855700f24f492e6
1il1day <jgffdet@nate.com>
2025-01-12 15:47:20 +0900

    react 초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
+// App.tsx
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
+type Props = {}
+
+export default function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TestButton>Test</TestButton>
     </div>
-  );
+  )
 }
 
-export default App;
+const TestButton = styled.button`
+  background-color: skyblue;
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: none;
+`;
